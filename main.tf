f1dcc63be8a31fdd98488419d93f050d5b1dc135
Idrisadisa <166888327+Idrisadisa@users.noreply.github.com>
2025-08-23 19:47:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -235,12 +235,13 @@ resource "aws_eks_node_group" "powerdevops" {
   subnet_ids      = aws_subnet.powerdevops_subnet[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
-  instance_types = ["c7i-flex.large"]
+  instance_types = ["t3.micro"]
+  
 
   remote_access {
     ec2_ssh_key               = var.ssh_key_name
